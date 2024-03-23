7f13f0f26827c509221e57467823ea4f1b41f1f2
Aliance <ralantosoalalatiana@gmail.com>
2024-03-23 09:39:03 +0300

    Changement de progress en Skill

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,90 +1,81 @@
-import React from "react";
+import React, { Component } from "react";
 import '../Css/Skills.css';
 
 
-function Education() {
-    return(
-        <section className="colorlib-skills" data-section="skills">
-            <div className="colorlib-narrow-content">
-                <div className="row">
-                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
-                        <span className="heading-meta">My Specialty</span>
-                        <h2 className="colorlib-heading animate-box">My Skills</h2>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-12 animate-box" data-animate-effect="fadeInLeft">
-                        <p>The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt and made herself on the way.</p>
-                    </div>
-                    <div className="col-md-6 animate-box" data-animate-effect="fadeInLeft">
-                        <div className="progress-wrap">
-                            <h3>Photoshop</h3>
-                            <div className="progress">
-                                <div className="progress-bar color-1" role="progressbar" aria-valuenow="75"
-                                aria-valuemin="0" aria-valuemax="100" width="75%">
-                                <span>75%</span>
-                                </div>
-                            </div>
+class Education extends Component{
+    render() {
+        return (
+            <section className="colorlib-skills" data-section="skills">
+                <div className="colorlib-narrow-content">
+                    <div className="row">
+                        <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                            <span className="heading-meta">My Specialty</span>
+                            <h2 className="colorlib-heading animate-box">My Skills</h2>
                         </div>
                     </div>
-                    <div className="col-md-6 animate-box" data-animate-effect="fadeInRight">
-                        <div className="progress-wrap">
-                            <h3>jQuery</h3>
-                            <div className="progress">
-                                <div className="progress-bar color-2" role="progressbar" aria-valuenow="60"
-                                aria-valuemin="0" aria-valuemax="100" width="60%">
-                                <span>60%</span>
+                    <div className="row">
+                        <div className="col-md-12 animate-box" data-animate-effect="fadeInLeft">
+                            <p>The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt and made herself on the way.</p>
+                        </div>
+                        
+                        <div className="row">
+                            <div className="col-md-6 animate-box p-4" data-animate-effect="fadeInLeft">
+                                <div className="progress-wrap">
+                                    <h3>Photoshop <span>75%</span></h3>
+                                    <div className="form-group ">
+                                        <input type="range" class="form-control-range custom-range" id="formControlRange" min="0" max="100" value="75"/>
+                                    </div>
+                                </div>
+                                <div className="progress-wrap">
+                                    <h3>Photoshop <span>75%</span></h3>
+                                    <div className="form-group ">
+                                        <input type="range" class="form-control-range custom-range" id="formControlRange" min="0" max="100" value="75"/>
+                                    </div>
+                                </div>
+                                <div className="progress-wrap">
+                                    <h3>Photoshop <span>75%</span></h3>
+                                    <div className="form-group ">
+                                        <input type="range" class="form-control-range custom-range" id="formControlRange" min="0" max="100" value="75"/>
+                                    </div>
+                                </div>
+                                <div className="progress-wrap">
+                                    <h3>Photoshop <span>75%</span></h3>
+                                    <div className="form-group ">
+                                        <input type="range" class="form-control-range custom-range" id="formControlRange" min="0" max="100" value="75"/>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                    <div className="col-md-6 animate-box" data-animate-effect="fadeInLeft">
-                        <div className="progress-wrap">
-                            <h3>HTML5</h3>
-                            <div className="progress">
-                                <div className="progress-bar color-3" role="progressbar" aria-valuenow="85"
-                                aria-valuemin="0" aria-valuemax="100" width="85%">
-                                <span>85%</span>
+                            <div className="col-md-6 animate-box p-4" data-animate-effect="fadeInLeft">
+                                <div className="progress-wrap">
+                                    <h3>Photoshop <span>75%</span></h3>
+                                    <div className="form-group ">
+                                        <input type="range" class="form-control-range custom-range" id="formControlRange" min="0" max="100" value="75"/>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-6 animate-box" data-animate-effect="fadeInRight">
-                        <div className="progress-wrap">
-                            <h3>CSS3</h3>
-                            <div className="progress">
-                                <div className="progress-bar color-4" role="progressbar" aria-valuenow="90"
-                                aria-valuemin="0" aria-valuemax="100" width="90%">
-                                <span>90%</span>
+                                <div className="progress-wrap">
+                                    <h3>Photoshop <span>75%</span></h3>
+                                    <div className="form-group ">
+                                        <input type="range" class="form-control-range custom-range" id="formControlRange" min="0" max="100" value="75"/>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-6 animate-box" data-animate-effect="fadeInLeft">
-                        <div className="progress-wrap">
-                            <h3>WordPress</h3>
-                            <div className="progress">
-                                <div className="progress-bar color-5" role="progressbar" aria-valuenow="70"
-                                aria-valuemin="0" aria-valuemax="100" width="70%">
-                                <span>70%</span>
+                                <div className="progress-wrap">
+                                    <h3>Photoshop <span>75%</span></h3>
+                                    <div className="form-group ">
+                                        <input type="range" class="form-control-range custom-range" id="formControlRange" min="0" max="100" value="75"/>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-6 animate-box" data-animate-effect="fadeInRight">
-                        <div className="progress-wrap">
-                            <h3>SEO</h3>
-                            <div className="progress">
-                                <div className="progress-bar color-6" role="progressbar" aria-valuenow="80"
-                                aria-valuemin="0" aria-valuemax="100" width="80%">
-                                <span>80%</span>
+                                <div className="progress-wrap">
+                                    <h3>Photoshop <span>75%</span></h3>
+                                    <div className="form-group ">
+                                        <input type="range" class="form-control-range custom-range" id="formControlRange" min="0" max="100" value="75"/>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
-    )
+            </section>
+        );
+    }
 }
 export default Education;
